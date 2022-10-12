aff3afa4540df2bc82abe15b87cc1241d7550175
Complejo Educativo "Pedro F. Cantor <114362661+cantorweb@users.noreply.github.com>
2022-10-12 14:06:08 -0600

    Add files via upload

---
+++ data/bd_logros.js
@@ -0,0 +1,20 @@
+let Logros = [
+    {
+        titulo:"titulo",
+        año:"2022",
+        contenido:"He waspring) in Salonica, then an Ottoman city, now inGreece. His father Ali Riza, a customs official turned lumber merchant, died when Mustafawas still a boy. His mother Zubeyde, adevout and stro",
+        img:"https://i.ibb.co/tbFdtCT/slider3.png",
+    },
+        {
+        titulo:"ejemplo",
+        año:"2021",
+        contenido:"He was born in 1881 (probably in the spring) in Salonica, then an Ottoman city, now inGreece. His father Ali Riza, a customs official turned lumber merchant, died when Mustafawas still a boy. His mother Zubeyde, adevout and stro",
+        img:"https://i.ibb.co/tbFdtCT/slider3.png",
+    },
+         {
+        titulo:"ejemplo",
+        año:"2019",
+        contenido:"He was born in 1881 (probably in the spring) in Salonica, then an Ottoman city, now inGreece. His father Ali Riza, a customs official turned lumber merchant, died when Mustafawas still a boy. His mother Zubeyde, adevout and stro",
+        img:"https://i.ibb.co/tbFdtCT/slider3.png",
+    },
+]
---
+++ data/bd_noticias.js
@@ -0,0 +1,30 @@
+const noticias = [
+    {
+        title:"Revista de logros",
+        subtitle:"CANTOR 2022",
+        p:"Revista de logros académicos del año 2022. Elaborada como parte de las actividades en proyecto de graduación de Tercero Contador 'A'",
+        info:[" 3° Contador 'A'", " 21/10/22"],
+        leermas:true,
+        oculto:`Parte esencial de nuestra visión con los alumnos es poner en practica lo aprendido, en este caso estudiantes de Tercero Contador Diseñaron y redactaron esta revista de logros académicos 2022 <br>  <a href="../pages/publicaciones/revista.html"> Ver revista <i class='bx bx-window-open'></i> </a> `,
+        img:"../img/noticias/revista.png"
+
+    },
+    {
+        title:"Noticia 2",
+        subtitle:"Lorem ip",
+        p:"Lorem ipsum dolor sad aliquam facilis numquam. Veritatis, sit",
+        img:"https://i.ibb.co/tbFdtCT/slider3.pnghttps://i.ibb.co/qC6qw1M/Carlos.png",
+        info:["BACHILLERATO", "30/10/21"],
+        leermas:true,
+        oculto:"lorem orem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos sit ad assumenda iste architecto culpa at aut reiciendis necessitatibus molestiae harum, iure error officiis enim debitis repudiandae minima fugit saepe. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti quam laboriosam et adipisci, natus velit rerum harum facilis modi ut totam nobis id necessitatibus labore numquam laborum cum accusamus err"
+    },
+        {
+        title:"Noticia 3",
+        subtitle:"Lorem ip",
+        p:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad eum dolorum architecto obcaecati enim dicta praesentium, quam nobis! Neque ad aliquam facilis numquam. Veritatis, sit",
+        img:"https://scontent.fsal4-1.fna.fbcdn.net/v/t39.30808-6/301213076_640278750998857_2087659786535011085_n.jpg?stp=dst-jpg_p640x640&_nc_cat=100&ccb=1-7&_nc_sid=e3f864&_nc_eui2=AeHPRRfkFMpy1RUiK5WsB6bG7WM_-tq7ATHtYz_62rsBMbPrlUgDQmKzHFRQ3AkcCo7JJlbKlDwdX0HHaK0dCe-h&_nc_ohc=iNStgjwLPZEAX_REpH5&_nc_oc=AQm9zs3AAMxX2frEn9pwnrr-ExIxufkw60CubCjdHepvEzJ9fnij2DjM8TqQfSBTppU&_nc_zt=23&_nc_ht=scontent.fsal4-1.fna&oh=00_AT-p77_HOkDjGZoYZ60WK0H9lEHQ6fnMGU9QsWVjBg0jnw&oe=631D3D2D",
+        info:["BACHILLERATO", "30/10/21"],
+        leermas:true,
+        oculto:"lorem orem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos sit ad assumenda iste architecto culpa at aut reiciendis necessitatibus molestiae harum, iure error officiis enim debitis repudiandae minima fugit saepe. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti quam laboriosam et adipisci, natus velit rerum harum facilis modi ut totam nobis id necessitatibus labore numquam laborum cum accusamus err"
+    }
+]
