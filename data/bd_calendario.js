cf12e5dc12396f79320dc04e9b0d46d65b12ba4d
Complejo Educativo "Pedro F. Cantor <114362661+cantorweb@users.noreply.github.com>
2022-10-31 14:38:07 -0600

    Update bd_calendario.js

--- data/bd_calendario.js
+++ data/bd_calendario.js
@@ -1,17 +1,9 @@
 const calendario = [ 
     //! FAVOR ESCRIBIR LA FECHA EN FOMRATO   A Ñ O - M E S - D I A
-    {
-        fecha:"2022-10-31",
-        titulo:"Fin año lectivo 2022",
-        desc: "Ultimo día de clases ordinarias"
-    }
-    ,
 
-    {
-        fecha:"2022-10-21",
-        titulo:"Feria de Logros",
-        desc:"Exposición de logros - año 2022",
-    },
+
+
+
     {
         fecha:"2022-11-16",
         titulo:"CLAUSURA",
